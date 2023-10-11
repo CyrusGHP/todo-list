774a4fabb34c5b8980365ae391cebddff3712158
CyrusGHP <78340590+CyrusGHP@users.noreply.github.com>
2023-10-11 08:21:13 +0800

    vite config fix again

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://cyrusghp.github.io/"
+  base: "https://cyrusghp.github.io/todo-list/",
 });
